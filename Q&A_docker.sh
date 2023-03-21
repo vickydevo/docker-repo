f0b5b35754f349e50de31dc7170ebaa0b88d91bd
melky <melky@gmail.com>
2023-03-21 20:44:01 +0530

    interviewQ file added

---
+++ Q&A_docker.sh
@@ -0,0 +1,21 @@
+# what is docker?
+docker is an open source containerization service.
+it enables developers to package applicationsinto containers.
+
+# what is a Docker container and how is it different from other a Virtual Machine?
+
+# what is a Dockerfile?
+a set of instructionsor or commands that difines a dcoker image and what dependencies should be installedfor the application to run
+Docker images act as a set of instructions to build a Docker container it can be compared to a snapshot in a VM 
+
+# what are the different Docker components?
+    1.Docker client
+    2.Docker Host 
+    3. Docker Registry 
+
+# what is the diference between docker and docker COPY and ADD?
+ Docker ADD can copy the files from a URL unlike 
+ Docker COPY which can only copy the files from host system to th container 
+
+ # what is the diference between CMD and ENTRYPOINT?
+ 
