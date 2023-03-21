969b611d526c3dd7ce9781694b773b6f93c8d84a
melky <melky@gmail.com>
2023-03-21 11:52:52 +0530

    Merge branch 'master' of https://github.com/vickydevo/docker-repo

---
+++ trietree/dockcmd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo docker search postgres
+sudo docker pull postgres:15.2
+sudo docker images
+docker run -d \
+	--name some-postgres \
+	-e POSTGRES_PASSWORD=mysecretpassword \
+	-e PGDATA=/var/lib/postgresql/data/pgdata \
+	-v /custom/mount:/var/lib/postgresql/data \
+	postgres
+    sudo docker build -t type-image-name .
+    sudo docker pull images
+    sudo run -d -p5000:6673 --name postgrescont postgres:15.2
+    sudo docker images
+    sudo docker stop container-name
+    sudo docker start container-name
+    sudo docker rm container-name
+    sudo docker rmi image-name
+    sudo docker exec -it container-name /bin/bash
+    sudo docker logs cointainer-name
