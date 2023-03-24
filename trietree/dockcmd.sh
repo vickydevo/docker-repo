53575ecc6d6baf929eacd6f08f16f9438c24e80d
john Doe <vignan.kandelab@gmail>
2023-03-24 04:20:06 +0000

    network definitions

--- trietree/dockcmd.sh
+++ trietree/dockcmd.sh
@@ -20,7 +20,18 @@ docker run -d \
     sudo docker logs cointainer-name
 # DOCKER NETWORK
    1.BridgesNetwork 
-    
+    this the default network which allows your network in standlone containers
 
-   2.HostNetwork
-   3.NoneNetwork
+    In terms of Docker, a bridge network uses a software bridge which allows containers connected to the same bridge network to communicate, 
+    while providing isolation from containers which are not connected to that bridge network.
+   
+   2.HostNetwork Docker host networking, is a networking mode in 
+   which a Docker container shares its network namespace with the host machine. 
+   we can use only in docker swarm
+
+   3.OverlayNetwork : The overlay network driver creates a distributed network among multiple Docker daemon hosts.
+    This network sits on top of (overlays) the host-specific networks, allowing containers connected to it
+    (including swarm service containers) to communicate securely when encryption is enabled
+
+    4.NoneNetwork: it completely disable the network
+    5.Macvlan : it assigns Mac addres to container. it is like physical device .it is used only for legacy application
